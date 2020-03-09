62f4da15a0de77c6247a4e666b218a923808e837
Myo Thant Kyaw <myothantkyaw21@gmail.com>
2020-03-09 10:36:32 +0630

    chapter 1 intro

---
+++ scripts/ch1_intro.js
@@ -0,0 +1,9 @@
+// var imported = document.createElement('script');
+// imported.src = '../scripts/ch1_3ds.js';
+// document.head.appendChild(imported);
+
+Ch1_3ds.getFig("fig_1_1No_1", "http://192.168.1.9:3000/ice_cream.glb");
+Ch1_3ds.getFig("fig_1_1No_2", "http://192.168.1.9:3000/soccer.glb");
+
+
+
