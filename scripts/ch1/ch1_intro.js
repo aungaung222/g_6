be2850bf8bac7cd92c6cc3ea5e8130789f5aa731
Myo Thant Kyaw <myothantkyaw21@gmail.com>
2020-03-10 19:09:15 +0630

    ..

--- scripts/ch1/ch1_intro.js
+++ scripts/ch1/ch1_intro.js
@@ -8,6 +8,9 @@
 Ch1_3ds.getCuboid("fig_1_1No_1");
 Ch1_3ds.getCube("fig_1_1No_2");
 Ch1_3ds.getSphere("fig_1_1No_3");
+Ch1_3ds.getCylinder("fig_1_1No_4");
+Ch1_3ds.getCone("fig_1_1No_5");
+Ch1_3ds.getPyramid("fig_1_1No_6");
 
 
 
