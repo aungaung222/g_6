af8b26eccb8de9cc207b91d87d3d9858b70a54df
Myo Thant Kyaw <myothantkyaw21@gmail.com>
2020-03-12 16:16:46 +0630

    ..

--- scripts/ch1/title_1_4.js
+++ scripts/ch1/title_1_4.js
@@ -1,2 +1,3 @@
-Title_1_4.getFig1_6_i("fig-img-i")
-Title_1_4.getFig1_6_ii("fig-img-ii")
+Title_1_4.getFig1_6_i("fig-img-i");
+Title_1_4.getFig1_6_ii("fig-img-ii");
+Title_1_4.getFig1_7_sphere("fig-img-1-7");
