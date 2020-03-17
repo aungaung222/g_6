6c1abb714b0e6eea7af635f0b40f20d2b3e46d1c
Myo Thant Kyaw <myothantkyaw21@gmail.com>
2020-03-17 19:25:58 +0630

    title 1.6

--- scripts/ch1/title_1_6.js
+++ scripts/ch1/title_1_6.js
@@ -1,3 +1,4 @@
 Title_1_6.getFig1_10("fig-img-1-10", "slider-fig-1-10")
 Title_1_6.getFig1_11("fig-img-1-11", "slider-fig-1-11")
+Title_1_6.getFig1_12("fig-img-1-12", "slider-fig-1-12")
 Title_1_6.getFig1_13("fig-img-1-13", "slider-fig-1-13")
