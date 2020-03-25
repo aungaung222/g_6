7555ccdff42b7a97f8a1ebdd0cfb06fc7a0d2873
Myo Thant Kyaw <myothantkyaw21@gmail.com>
2020-03-25 19:48:41 +0630

    aa

--- scripts/common_actions.js
+++ scripts/common_actions.js
@@ -17,7 +17,7 @@ var gifs = document.getElementsByClassName("vid-gif");
 
 for (var i = 0; i < gifs.length; i++) {
   gifs[i].playing = false;
-  gifs[i].loop = true;
+  gifs[i].loop = false;
   gifs[i].style.borderRadius = "6px"
   var color = gifs[i].getAttribute("data-color");
 
@@ -42,7 +42,7 @@ for (var i = 0; i < gifs.length; i++) {
   play_icon.style.width = size + "px"
   play_icon.style.height = size + "px"
   play_icon.style.left = "50%";
-  play_icon.style.top = "50%";
+  play_icon.style.top = "48%";
   play_icon.style.zIndex = 3;
   play_icon.style.transform = "translate(-50%, -50%)";
   gifs[i].play_icon = play_icon;
@@ -70,89 +70,94 @@ for (var i = 0; i < gifs.length; i++) {
       this.playing = false;
     }
   }, false);
+
+  gifs[i].addEventListener("ended", function () {
+    $(this.play_icon).show(100);
+    this.playing = false;
+  }, false);
 }
 
 
 
 var sliders = []
-window.jssor_1_slider_init = function() {
+window.jssor_1_slider_init = function () {
 
-    var jssor_1_SlideshowTransitions = [
-      {$Duration:800,x:0.25,$Zoom:1.5,$Easing:{$Left:$Jease$.$InWave,$Zoom:$Jease$.$InCubic},$Opacity:2,$ZIndex:-10,$Brother:{$Duration:800,x:-0.25,$Zoom:1.5,$Easing:{$Left:$Jease$.$InWave,$Zoom:$Jease$.$InCubic},$Opacity:2,$ZIndex:-10}},
-      {$Duration:1200,x:0.5,$Cols:2,$ChessMode:{$Column:3},$Easing:{$Left:$Jease$.$InOutCubic},$Opacity:2,$Brother:{$Duration:1200,$Opacity:2}},
-      {$Duration:600,x:0.3,$During:{$Left:[0.6,0.4]},$Easing:{$Left:$Jease$.$InCubic,$Opacity:$Jease$.$Linear},$Opacity:2,$Brother:{$Duration:600,x:-0.3,$Easing:{$Left:$Jease$.$InCubic,$Opacity:$Jease$.$Linear},$Opacity:2}},
-      {$Duration:800,x:0.25,y:0.5,$Rotate:-0.1,$Easing:{$Left:$Jease$.$InQuad,$Top:$Jease$.$InQuad,$Opacity:$Jease$.$Linear,$Rotate:$Jease$.$InQuad},$Opacity:2,$Brother:{$Duration:800,x:-0.1,y:-0.7,$Rotate:0.1,$Easing:{$Left:$Jease$.$InQuad,$Top:$Jease$.$InQuad,$Opacity:$Jease$.$Linear,$Rotate:$Jease$.$InQuad},$Opacity:2}},
-      {$Duration:1000,x:1,$Rows:2,$ChessMode:{$Row:3},$Easing:{$Left:$Jease$.$InOutQuart,$Opacity:$Jease$.$Linear},$Opacity:2,$Brother:{$Duration:1000,x:-1,$Rows:2,$ChessMode:{$Row:3},$Easing:{$Left:$Jease$.$InOutQuart,$Opacity:$Jease$.$Linear},$Opacity:2}},
-      {$Duration:1000,y:-1,$Cols:2,$ChessMode:{$Column:12},$Easing:{$Top:$Jease$.$InOutQuart,$Opacity:$Jease$.$Linear},$Opacity:2,$Brother:{$Duration:1000,y:1,$Cols:2,$ChessMode:{$Column:12},$Easing:{$Top:$Jease$.$InOutQuart,$Opacity:$Jease$.$Linear},$Opacity:2}},
-      {$Duration:800,y:1,$Easing:{$Top:$Jease$.$InOutQuart,$Opacity:$Jease$.$Linear},$Opacity:2,$Brother:{$Duration:800,y:-1,$Easing:{$Top:$Jease$.$InOutQuart,$Opacity:$Jease$.$Linear},$Opacity:2}},
-      {$Duration:1000,x:-0.1,y:-0.7,$Rotate:0.1,$During:{$Left:[0.6,0.4],$Top:[0.6,0.4],$Rotate:[0.6,0.4]},$Easing:{$Left:$Jease$.$InQuad,$Top:$Jease$.$InQuad,$Opacity:$Jease$.$Linear,$Rotate:$Jease$.$InQuad},$Opacity:2,$Brother:{$Duration:1000,x:0.2,y:0.5,$Rotate:-0.1,$Easing:{$Left:$Jease$.$InQuad,$Top:$Jease$.$InQuad,$Opacity:$Jease$.$Linear,$Rotate:$Jease$.$InQuad},$Opacity:2}},
-      {$Duration:800,x:-0.2,$Delay:40,$Cols:12,$During:{$Left:[0.4,0.6]},$SlideOut:true,$Formation:$JssorSlideshowFormations$.$FormationStraight,$Assembly:260,$Easing:{$Left:$Jease$.$InOutExpo,$Opacity:$Jease$.$InOutQuad},$Opacity:2,$Outside:true,$Round:{$Top:0.5},$Brother:{$Duration:800,x:0.2,$Delay:40,$Cols:12,$Formation:$JssorSlideshowFormations$.$FormationStraight,$Assembly:1028,$Easing:{$Left:$Jease$.$InOutExpo,$Opacity:$Jease$.$InOutQuad},$Opacity:2,$Round:{$Top:0.5},$Shift:-200}},
-      {$Duration:700,$Opacity:2,$Brother:{$Duration:700,$Opacity:2}},
-      {$Duration:800,x:1,$Easing:{$Left:$Jease$.$InOutQuart,$Opacity:$Jease$.$Linear},$Opacity:2,$Brother:{$Duration:800,x:-1,$Easing:{$Left:$Jease$.$InOutQuart,$Opacity:$Jease$.$Linear},$Opacity:2}}
-    ];
+  var jssor_1_SlideshowTransitions = [
+    { $Duration: 800, x: 0.25, $Zoom: 1.5, $Easing: { $Left: $Jease$.$InWave, $Zoom: $Jease$.$InCubic }, $Opacity: 2, $ZIndex: -10, $Brother: { $Duration: 800, x: -0.25, $Zoom: 1.5, $Easing: { $Left: $Jease$.$InWave, $Zoom: $Jease$.$InCubic }, $Opacity: 2, $ZIndex: -10 } },
+    { $Duration: 1200, x: 0.5, $Cols: 2, $ChessMode: { $Column: 3 }, $Easing: { $Left: $Jease$.$InOutCubic }, $Opacity: 2, $Brother: { $Duration: 1200, $Opacity: 2 } },
+    { $Duration: 600, x: 0.3, $During: { $Left: [0.6, 0.4] }, $Easing: { $Left: $Jease$.$InCubic, $Opacity: $Jease$.$Linear }, $Opacity: 2, $Brother: { $Duration: 600, x: -0.3, $Easing: { $Left: $Jease$.$InCubic, $Opacity: $Jease$.$Linear }, $Opacity: 2 } },
+    { $Duration: 800, x: 0.25, y: 0.5, $Rotate: -0.1, $Easing: { $Left: $Jease$.$InQuad, $Top: $Jease$.$InQuad, $Opacity: $Jease$.$Linear, $Rotate: $Jease$.$InQuad }, $Opacity: 2, $Brother: { $Duration: 800, x: -0.1, y: -0.7, $Rotate: 0.1, $Easing: { $Left: $Jease$.$InQuad, $Top: $Jease$.$InQuad, $Opacity: $Jease$.$Linear, $Rotate: $Jease$.$InQuad }, $Opacity: 2 } },
+    { $Duration: 1000, x: 1, $Rows: 2, $ChessMode: { $Row: 3 }, $Easing: { $Left: $Jease$.$InOutQuart, $Opacity: $Jease$.$Linear }, $Opacity: 2, $Brother: { $Duration: 1000, x: -1, $Rows: 2, $ChessMode: { $Row: 3 }, $Easing: { $Left: $Jease$.$InOutQuart, $Opacity: $Jease$.$Linear }, $Opacity: 2 } },
+    { $Duration: 1000, y: -1, $Cols: 2, $ChessMode: { $Column: 12 }, $Easing: { $Top: $Jease$.$InOutQuart, $Opacity: $Jease$.$Linear }, $Opacity: 2, $Brother: { $Duration: 1000, y: 1, $Cols: 2, $ChessMode: { $Column: 12 }, $Easing: { $Top: $Jease$.$InOutQuart, $Opacity: $Jease$.$Linear }, $Opacity: 2 } },
+    { $Duration: 800, y: 1, $Easing: { $Top: $Jease$.$InOutQuart, $Opacity: $Jease$.$Linear }, $Opacity: 2, $Brother: { $Duration: 800, y: -1, $Easing: { $Top: $Jease$.$InOutQuart, $Opacity: $Jease$.$Linear }, $Opacity: 2 } },
+    { $Duration: 1000, x: -0.1, y: -0.7, $Rotate: 0.1, $During: { $Left: [0.6, 0.4], $Top: [0.6, 0.4], $Rotate: [0.6, 0.4] }, $Easing: { $Left: $Jease$.$InQuad, $Top: $Jease$.$InQuad, $Opacity: $Jease$.$Linear, $Rotate: $Jease$.$InQuad }, $Opacity: 2, $Brother: { $Duration: 1000, x: 0.2, y: 0.5, $Rotate: -0.1, $Easing: { $Left: $Jease$.$InQuad, $Top: $Jease$.$InQuad, $Opacity: $Jease$.$Linear, $Rotate: $Jease$.$InQuad }, $Opacity: 2 } },
+    { $Duration: 800, x: -0.2, $Delay: 40, $Cols: 12, $During: { $Left: [0.4, 0.6] }, $SlideOut: true, $Formation: $JssorSlideshowFormations$.$FormationStraight, $Assembly: 260, $Easing: { $Left: $Jease$.$InOutExpo, $Opacity: $Jease$.$InOutQuad }, $Opacity: 2, $Outside: true, $Round: { $Top: 0.5 }, $Brother: { $Duration: 800, x: 0.2, $Delay: 40, $Cols: 12, $Formation: $JssorSlideshowFormations$.$FormationStraight, $Assembly: 1028, $Easing: { $Left: $Jease$.$InOutExpo, $Opacity: $Jease$.$InOutQuad }, $Opacity: 2, $Round: { $Top: 0.5 }, $Shift: -200 } },
+    { $Duration: 700, $Opacity: 2, $Brother: { $Duration: 700, $Opacity: 2 } },
+    { $Duration: 800, x: 1, $Easing: { $Left: $Jease$.$InOutQuart, $Opacity: $Jease$.$Linear }, $Opacity: 2, $Brother: { $Duration: 800, x: -1, $Easing: { $Left: $Jease$.$InOutQuart, $Opacity: $Jease$.$Linear }, $Opacity: 2 } }
+  ];
 
-    var jssor_1_options = {
-      $AutoPlay: 0,
-      $FillMode: 5,
-      $SlideshowOptions: {
-        $Class: $JssorSlideshowRunner$,
-        $Transitions: jssor_1_SlideshowTransitions,
-        $TransitionsOrder: 1
-      },
-      $ArrowNavigatorOptions: {
-        $Class: $JssorArrowNavigator$
-      },
-      $BulletNavigatorOptions: {
-        $Class: $JssorBulletNavigator$,
-        $SpacingX: 16,
-        $SpacingY: 16
+  var jssor_1_options = {
+    $AutoPlay: 0,
+    $FillMode: 5,
+    $SlideshowOptions: {
+      $Class: $JssorSlideshowRunner$,
+      $Transitions: jssor_1_SlideshowTransitions,
+      $TransitionsOrder: 1
+    },
+    $ArrowNavigatorOptions: {
+      $Class: $JssorArrowNavigator$
+    },
+    $BulletNavigatorOptions: {
+      $Class: $JssorBulletNavigator$,
+      $SpacingX: 16,
+      $SpacingY: 16
+    }
+  };
+
+  var class_sliders = document.getElementsByClassName("gallery-slider");
+  for (var i = 0; i < class_sliders.length; i++) {
+
+    var jssor_1_slider = new $JssorSlider$(class_sliders[i].id, jssor_1_options);
+
+    class_sliders[i].style.marginTop = "-50px";
+    class_sliders[i].style.marginBottom = "-10px";
+    // class_sliders[i].style.width = "100%";
+    // class_sliders[i].style.maxHeight = "200px"
+    // class_sliders[i].style.padding = "0px"
+
+    sliders.push(jssor_1_slider)
+  }
+
+
+  /*#region responsive code begin*/
+
+  var MAX_WIDTH = 600;
+
+  function ScaleSlider() {
+
+    for (var i = 0; i < sliders.length; i++) {
+      var containerElement = sliders[i].$Elmt.parentNode;
+      var containerWidth = containerElement.clientWidth;
+
+      if (containerWidth) {
+
+        var expectedWidth = Math.min(MAX_WIDTH || containerWidth, containerWidth);
+
+        sliders[i].$ScaleWidth(expectedWidth);
       }
-    };
-
-    var class_sliders = document.getElementsByClassName("gallery-slider");
-    for(var i = 0; i < class_sliders.length; i++){
-        
-        var jssor_1_slider = new $JssorSlider$(class_sliders[i].id, jssor_1_options);
-        
-        class_sliders[i].style.marginTop = "-50px";
-        class_sliders[i].style.marginBottom = "-10px";
-        // class_sliders[i].style.width = "100%";
-        // class_sliders[i].style.maxHeight = "200px"
-        // class_sliders[i].style.padding = "0px"
-        
-        sliders.push(jssor_1_slider)
-    }
-
-
-    /*#region responsive code begin*/
-
-    var MAX_WIDTH = 600;
-
-    function ScaleSlider() {
-
-        for(var i = 0; i < sliders.length; i++){
-            var containerElement = sliders[i].$Elmt.parentNode;
-        var containerWidth = containerElement.clientWidth;
-
-        if (containerWidth) {
-
-            var expectedWidth = Math.min(MAX_WIDTH || containerWidth, containerWidth);
-
-            sliders[i].$ScaleWidth(expectedWidth);
-        }
-        else {
-            window.setTimeout(ScaleSlider, 30);
-        }
-
-        }
+      else {
+        window.setTimeout(ScaleSlider, 30);
+      }
 
     }
 
-    ScaleSlider();
+  }
 
-    $Jssor$.$AddEvent(window, "load", ScaleSlider);
-    $Jssor$.$AddEvent(window, "resize", ScaleSlider);
-    $Jssor$.$AddEvent(window, "orientationchange", ScaleSlider);
-    /*#endregion responsive code end*/
+  ScaleSlider();
+
+  $Jssor$.$AddEvent(window, "load", ScaleSlider);
+  $Jssor$.$AddEvent(window, "resize", ScaleSlider);
+  $Jssor$.$AddEvent(window, "orientationchange", ScaleSlider);
+  /*#endregion responsive code end*/
 };
 jssor_1_slider_init()
